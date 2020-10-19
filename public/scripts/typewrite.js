5c63e9c8da875fb543c73132c53140a4e677b705
Jucelania Silva <juselania98@outlook.com>
2020-10-19 04:04:10 -0300

    Update typewrite.js

--- public/scripts/typewrite.js
+++ public/scripts/typewrite.js
@@ -8,18 +8,4 @@
       }
      setTimeout(digitaTexto(element),2000); */
 
-     function typeWrite(elemento){
-        const textoArray = elemento.innerHTML.split('');
-        elemento.innerHTML = ' ';
-        textoArray.forEach(function(letra, i){   
-          
-        setTimeout(function(){
-            elemento.innerHTML += letra;
-        }, 75 * i)
-    
-      });
-    }
-    const titulo = document.querySelector('h1');
-    typeWrite(titulo);
-    
-    typeWrite(document.querySelector('#animate'));
+     
